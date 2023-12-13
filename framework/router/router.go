959b027b985c767cc04bedfa2aae51d17e73c9d1
Galvao <rafaeloliveira@febracis.com.br>
2023-12-13 02:13:08 -0300

    add routers

---
+++ framework/router/router.go
@@ -0,0 +1,105 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/rafael-ogsantos/eulabs-api/application/repositories"
+	"github.com/rafael-ogsantos/eulabs-api/application/services"
+	"github.com/rafael-ogsantos/eulabs-api/domain"
+	"gorm.io/gorm"
+)
+
+func New(conn *gorm.DB) *echo.Echo {
+	e := echo.New()
+
+	// FindById
+	e.GET("/product/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		ctx := c.Request().Context()
+
+		productRepository := repositories.NewProductRepositoryDb(conn)
+		productService := services.NewProductService(productRepository)
+
+		product, err := productService.FindById(ctx, id)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, product)
+	})
+
+	// Create
+	e.POST("/product", func(c echo.Context) (err error) {
+		p := new(domain.Product)
+		if err = c.Bind(p); err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		productRepository := repositories.NewProductRepositoryDb(conn)
+		productService := services.NewProductService(productRepository)
+
+		product := &domain.Product{
+			Name:        p.Name,
+			Description: p.Description,
+		}
+
+		productCreated, err := productService.Insert(c.Request().Context(), product)
+
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, productCreated)
+	})
+
+	// Update
+	e.PUT("/product/:id", func(c echo.Context) error {
+		p := new(domain.Product)
+		id := c.Param("id")
+		ctx := c.Request().Context()
+
+		if err := c.Bind(p); err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		productRepository := repositories.NewProductRepositoryDb(conn)
+		productService := services.NewProductService(productRepository)
+
+		_, err := productService.FindById(ctx, id)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		product := &domain.Product{
+			ID:          id,
+			Name:        p.Name,
+			Description: p.Description,
+			CreatedAt:   p.CreatedAt,
+		}
+
+		productUpdated, err := productService.Update(c.Request().Context(), product)
+
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, productUpdated)
+	})
+
+	e.DELETE("/product/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		ctx := c.Request().Context()
+		productRepository := repositories.NewProductRepositoryDb(conn)
+		productService := services.NewProductService(productRepository)
+
+		err := productService.Delete(ctx, id)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, "Product deleted")
+	})
+
+	return e
+}
