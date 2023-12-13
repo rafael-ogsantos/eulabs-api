102b52d07ab5d8509c97468378e64ef342b93d29
Galvao <rafaeloliveira@febracis.com.br>
2023-12-13 02:11:53 -0300

    adjustment

--- domain/product.go
+++ domain/product.go
@@ -10,17 +10,13 @@ type Product struct {
 	ID          string    `valid:"uuid"`
 	Name        string    `valid:"required"`
 	Description string    `valid:"required"`
-	CreatedAt   time.Time `valid:"-"` // o "-" significa que não quero validar esse campo
+	CreatedAt   time.Time `valid:"-"`
 }
 
 func init() {
 	govalidator.SetFieldsRequiredByDefault(true)
 }
 
-// o * e o & são ponteiros de memória que apontam para o endereço de memória de uma variável
-// quando usado, eu altero o valor da variável original e não uma cópia dela, por exemplo se eu passar
-// um ponteiro de memória para uma função, e alterar o valor da variável dentro da função, quando eu sair da função, o valor da variável original
-// também terá sido alterado
 func NewProduct() *Product {
 	return &Product{}
 }
