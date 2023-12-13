ea34236b2ad21bf0f25a38ba384caa81f29e6ff8
Galvao <rafaeloliveira@febracis.com.br>
2023-12-13 02:12:48 -0300

    adjustment driver db

--- framework/database/db.go
+++ framework/database/db.go
@@ -3,9 +3,12 @@ package database
 import (
 	"log"
 
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/rafael-ogsantos/eulabs-api/domain"
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type Database struct {
@@ -26,17 +29,19 @@ func NewDatabase() *Database {
 func NewDatabaseTest() *gorm.DB {
 	dbInstance := NewDatabase()
 	dbInstance.Env = "test"
-	dbInstance.DbType = "sqlite3"
-	dbInstance.DsnTest = ":memory"
+	dbInstance.Dsn = "root:root@tcp(db:3306)/eulabs?charset=utf8&parseTime=True&loc=Local"
+	dbInstance.DsnTest = ":memory:"
+	dbInstance.DbType = "mysql"
+	dbInstance.DbTypeTest = "sqlite3"
 	dbInstance.AutoMigrateDb = true
+	dbInstance.Debug = true
 
-	connection, err := dbInstance.Connect()
-
+	db, err := dbInstance.Connect()
 	if err != nil {
-		log.Fatalf("Test db error: %v", err)
+		log.Fatal(err)
 	}
 
-	return connection
+	return db
 }
 
 func (d *Database) Connect() (*gorm.DB, error) {
@@ -44,12 +49,32 @@ func (d *Database) Connect() (*gorm.DB, error) {
 	var err error
 
 	if d.Env != "test" {
-		d.Db, err = gorm.Open(mysql.Open(d.Dsn))
+		d.Db, err = gorm.Open(mysql.New(mysql.Config{
+			DSN:                       d.Dsn,
+			DefaultStringSize:         256,
+			DisableDatetimePrecision:  true,
+			DontSupportRenameIndex:    true,
+			DontSupportRenameColumn:   true,
+			SkipInitializeWithVersion: false,
+		}), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Silent),
+		})
 	} else {
-		d.Db, err = gorm.Open(mysql.Open(d.DsnTest))
+		d.Db, err = gorm.Open(sqlite.Open(d.DsnTest), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Silent),
+		})
 	}
 
 	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := d.Db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	if err = sqlDB.Ping(); err != nil {
 		return nil, err
 	}
 
